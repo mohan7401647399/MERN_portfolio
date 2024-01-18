d56df89618393d7899c3716e35cd9e60ec5f9e13
Mohan R <127593802+mohan7401647399@users.noreply.github.com>
2024-01-18 22:44:02 +0530

    Update Projects.js

--- frontend/src/components/Projects.js
+++ frontend/src/components/Projects.js
@@ -45,7 +45,7 @@ export const Projects = () => {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
+      <img className="background-image-right" src={colorSharp2} alt="img"></img>
     </section>
   )
 }

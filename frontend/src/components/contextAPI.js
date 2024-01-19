660387269778d83f1bcca06852be706b0975c4ca
Mohan R <127593802+mohan7401647399@users.noreply.github.com>
2024-01-19 20:43:01 +0530

    Update contextAPI.js

--- frontend/src/components/contextAPI.js
+++ frontend/src/components/contextAPI.js
@@ -50,70 +50,70 @@ export default function UserContextProvider({ children }) {
     const toRotate = ["MERN stack Developer", "Web Designer", "UI/UX Designer"];
     const period = 2000;
 
-    // useEffect(() => {
-    //     let ticker = setInterval(() => {
-    //         tick();
-    //     }, delta);
+       const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
 
-    //     return () => { clearInterval(ticker) };
-    // }, [text,delta])
+        setText(updatedText);
 
-    // const tick = () => {
-    //     let i = loopNum % toRotate.length;
-    //     let fullText = toRotate[i];
-    //     let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-    //     setText(updatedText);
-
-    //     if (isDeleting) {
-    //         setDelta(prevDelta => prevDelta / 2);
-    //     }
-
-    //     if (!isDeleting && updatedText === fullText) {
-    //         setIsDeleting(true);
-    //         setIndex(prevIndex => prevIndex - 1);
-    //         setDelta(period);
-    //     } else if (isDeleting && updatedText === '') {
-    //         setIsDeleting(false);
-    //         setLoopNum(loopNum + 1);
-    //         setIndex(1);
-    //         setDelta(500);
-    //     } else {
-    //         setIndex(prevIndex => prevIndex + 1);
-    //     }
-    // }
-
-           useEffect(() => {
-        const tick = () => {
-            let i = loopNum % toRotate.length;
-            let fullText = toRotate[i];
-            let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
-
-            setText(updatedText);
-
-            if (isDeleting) {
-                setDelta(prevDelta => prevDelta / 2);
-            }
-
-            if (!isDeleting && updatedText === fullText) {
-                setIsDeleting(true);
-                setIndex(prevIndex => prevIndex - 1);
-                setDelta(period);
-            } else if (isDeleting && updatedText === '') {
-                setIsDeleting(false);
-                setLoopNum(loopNum + 1);
-                setIndex(1);
-                setDelta(500);
-            } else {
-                setIndex(prevIndex => prevIndex + 1);
-            }
+        if (isDeleting) {
+            setDelta(prevDelta => prevDelta / 2);
         }
+
+        if (!isDeleting && updatedText === fullText) {
+            setIsDeleting(true);
+            setIndex(prevIndex => prevIndex - 1);
+            setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setIndex(1);
+            setDelta(500);
+        } else {
+            setIndex(prevIndex => prevIndex + 1);
+        }
+    }
+
+    useEffect(() => {
         let ticker = setInterval(() => {
             tick();
         }, delta);
+
         return () => { clearInterval(ticker) };
-        // }, [text])
-    }, [text, delta, isDeleting, loopNum, toRotate])
+    }, [text])
+
+    //        useEffect(() => {
+    //     const tick = () => {
+    //         let i = loopNum % toRotate.length;
+    //         let fullText = toRotate[i];
+    //         let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+    //         setText(updatedText);
+
+    //         if (isDeleting) {
+    //             setDelta(prevDelta => prevDelta / 2);
+    //         }
+
+    //         if (!isDeleting && updatedText === fullText) {
+    //             setIsDeleting(true);
+    //             setIndex(prevIndex => prevIndex - 1);
+    //             setDelta(period);
+    //         } else if (isDeleting && updatedText === '') {
+    //             setIsDeleting(false);
+    //             setLoopNum(loopNum + 1);
+    //             setIndex(1);
+    //             setDelta(500);
+    //         } else {
+    //             setIndex(prevIndex => prevIndex + 1);
+    //         }
+    //     }
+    //     let ticker = setInterval(() => {
+    //         tick();
+    //     }, delta);
+    //     return () => { clearInterval(ticker) };
+    //     // }, [text])
+    // }, [text, delta, isDeleting, loopNum, toRotate])
 
     
     //Skills page functions

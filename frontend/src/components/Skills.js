5eae07dce8824f3dc15b76765c4a20436d84c540
Mohan R <127593802+mohan7401647399@users.noreply.github.com>
2024-01-19 06:20:31 +0530

    Update Skills.js

--- frontend/src/components/Skills.js
+++ frontend/src/components/Skills.js
@@ -42,7 +42,7 @@ export const Skills = () => {
                 </div>
             </div>
         </div>
-        <img className="background-image-left" src={colorSharp} alt="Image" />
+        <img className="background-image-left" src={colorSharp} alt="bg" />
     </section>
   )
 }

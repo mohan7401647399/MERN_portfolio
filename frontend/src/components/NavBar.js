514269f43d443f187b0078f582e13580febfc07a
Mohan R <127593802+mohan7401647399@users.noreply.github.com>
2024-01-25 07:11:27 +0530

    Update NavBar.js

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,5 +1,4 @@
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../assets/img/LOGO.jpg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
 import navIcon3 from '../assets/img/nav-icon3.svg';

43f1f3c1b67eedfb9f27903282dfb821ce5faf17
Mohan R <127593802+mohan7401647399@users.noreply.github.com>
2024-01-25 07:33:08 +0530

    Update Banner.js

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -23,7 +23,7 @@ export const Banner = () => {
                   <span className="m-3 p-2" style={{"fontSize":"35px"}}><a className="text-warning" href={resume} download="resume"><BsArrowDownSquareFill />
                   </a></span>
                   <h1>{`Hi! I'm Mohan`} <span className="txt-rotate"><span className="wrap">{text}</span></span></h1>
-                  <p>👋 Hello! I'm Mohan R, a passionate and detail-oriented Web Application Developer with a track record of creating powerful, secure, and user-friendly web solutions. I take pride in crafting robust applications that elevate businesses and empower users.
+                  <p>👋 Hello! I'm <span className="text-warning">Mohan R</span>, a passionate and detail-oriented Web Application Developer with a track record of creating powerful, secure, and user-friendly web solutions. I take pride in crafting robust applications that elevate businesses and empower users.
                   </p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
                 </div>}

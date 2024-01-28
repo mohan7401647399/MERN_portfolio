40c7cb56ad8ad8f990b9517d33bcb13ec5d2129d
Mohan R <127593802+mohan7401647399@users.noreply.github.com>
2024-01-28 22:18:27 +0530

    Update Banner.js

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -38,10 +38,6 @@ export const Banner = () => {
                     <div className="bannerimg">
                       <img className="m-auto p-auto" src={headerImg} alt="Header Img" />
                         <h2>MERN stack developer</h2>
-                        <Button variant="outline-primary">
-                            Hire me
-//                         <a href="#" className="text-white text-decoration-none">Hire Me</a>
-                        </Button>
                     </div>
                   </div>
                 </div>}

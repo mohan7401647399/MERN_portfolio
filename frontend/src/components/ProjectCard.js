82c446bb91dc69eb67c68bc5bdeec96f7c1f69cf
Mohan R <127593802+mohan7401647399@users.noreply.github.com>
2024-01-28 11:34:56 +0530

    Update ProjectCard.js

--- frontend/src/components/ProjectCard.js
+++ frontend/src/components/ProjectCard.js
@@ -18,3 +18,21 @@ export const ProjectCard = ({ title, link, imgUrl }) => {
     </Col>
   )
 }
+
+
+export const MernProjectsCard = ({ title, link, imgUrl }) => {
+  return (
+    <Col size={12} sm={6} md={4}>
+      <div className="proj-imgbx">
+        <img src={imgUrl} alt="img" />
+        <div className="proj-txtx">
+          <h4>{title}</h4>
+            <Button variant="outline-success">
+              <a className="text-white text-decoration-none" href={link}>Click here <BsLink />
+              </a>
+            </Button>
+        </div>
+      </div>
+    </Col>
+  )
+}

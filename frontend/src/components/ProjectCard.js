6aaad2e0a18a8e203aca1fe32b697d8c62c515ce
mohan7401647399 <mtm.kcs@gmail.com>
2024-08-17 14:07:43 +0530

    projectCard edited

--- frontend/src/components/ProjectCard.js
+++ frontend/src/components/ProjectCard.js
@@ -9,14 +9,20 @@ export const ProjectCard = ({ title, link, imgUrl, gitHub }) => {
     <Col size={12} xs={10} sm={6} md={4} className="m-auto">
       <div className="proj-imgbx" >
         <img src={imgUrl} alt="React-clones" />
-        <div className="proj-txtx d-grid w-auto">
+        <div className="proj-txtx d-grid">
           <h4>{title}</h4>
-          <Button variant="outline-success" className=" m-1">
-            <a className="text-white text-decoration-none" target="_blank" rel="noreferrer" href={link}>Live <BsLink />
+          <Button variant="outline-success" className="w-40 m-auto">
+            <a className="text-white text-decoration-none" target="_blank" rel="noreferrer" href={link}>
+              <span className="flex align-items-center text-center justify-evenly">
+                Live <BsLink />
+              </span>
             </a>
           </Button>
-          <Button variant="outline-success" className=" m-1">
-            <a className="text-white text-decoration-none" target="_blank" rel="noreferrer" href={gitHub}>GitHub <BsLink />
+          <Button variant="outline-success" className="w-40 m-auto">
+            <a className="text-white text-decoration-none" target="_blank" rel="noreferrer" href={gitHub}>
+              <span className="flex align-items-center text-center justify-evenly">
+                GitHub <BsLink />
+              </span>
             </a>
           </Button>
         </div>
@@ -32,14 +38,20 @@ export const MernProjectsCard = ({ title, link, imgUrl, gitHub }) => {
     <Col size={12} xs={10} sm={6} md={4} className="m-auto">
       <div className="proj-imgbx">
         <img src={imgUrl} alt="MERN-clones" />
-        <div className="proj-txtx d-grid w-auto">
+        <div className="proj-txtx d-grid">
           <h4>{title}</h4>
-          <Button variant="outline-success" className=" m-1">
-            <a className="text-white text-decoration-none" target="_blank" rel="noreferrer" href={link}>Live <BsLink />
+          <Button variant="outline-success" className="w-40 m-auto">
+            <a className="text-white text-decoration-none" target="_blank" rel="noreferrer" href={link}>
+              <span className="flex align-items-center text-center justify-evenly">
+                Live <BsLink />
+              </span>
             </a>
           </Button>
-          <Button variant="outline-success" className=" m-1">
-            <a className="text-white text-decoration-none" target="_blank" rel="noreferrer" href={gitHub}>Github<BsLink />
+          <Button variant="outline-success" className="w-40 m-auto">
+            <a className="text-white text-decoration-none" target="_blank" rel="noreferrer" href={gitHub}>
+              <span className="flex align-items-center text-center justify-evenly">
+                Github<BsLink />
+              </span>
             </a>
           </Button>
         </div>
@@ -55,14 +67,20 @@ export const JsGamesCard = ({ title, link, imgUrl, gitHub }) => {
     <Col size={12} xs={10} sm={6} md={4} className="m-auto">
       <div className="proj-imgbx">
         <img src={imgUrl} alt="JS_games" />
-        <div className="proj-txtx d-grid w-auto">
+        <div className="proj-txtx d-grid">
           <h4>{title}</h4>
-          <Button variant="outline-success" className=" m-1">
-            <a className="text-white text-decoration-none" target="_blank" rel="noreferrer" href={link}>Live<BsLink />
+          <Button variant="outline-success" className="w-40 m-auto">
+            <a className="text-white text-decoration-none" target="_blank" rel="noreferrer" href={link}>
+              <span className="flex align-items-center text-center justify-evenly">
+                Live<BsLink />
+              </span>
             </a>
           </Button>
-          <Button variant="outline-success" className=" m-1">
-            <a className="text-white text-decoration-none" target="_blank" rel="noreferrer" href={gitHub}>Github<BsLink />
+          <Button variant="outline-success" className="w-40 m-auto">
+            <a className="text-white text-decoration-none" target="_blank" rel="noreferrer" href={gitHub}>
+              <span className="flex align-items-center text-center justify-evenly">
+                Github<BsLink />
+              </span>
             </a>
           </Button>
         </div>
